330494efbbbe7d519b2ae990c409413877b1aa3f
Filipa Pedrosa <ana.pedrosa@airc.pt>
2016-04-15 16:30:06 +0100

    Criar o component article

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,26 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template:`
+    <form class="ui large form segment">
+    <h3 class="ui header">Add a Link</h3>
+    <div class="field">
+    <label for="title">Title:</label>
+    <input name="title" #newtitle>
+    </div>
+    <div class="field">
+    <label for="link">Link:</label>
+    <input name="link" #newlink>
+    </div>
+    <button (click)="addArticle(newtitle, newlink)"
+    class="ui positive right floated button">
+    Submit link
+    </button>
+    </form>`
 })
-export class AppComponent { }
+export class AppComponent {
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    }
+}
